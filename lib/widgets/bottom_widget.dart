d1cf1c666bdef0d6012bfbafc2f398320eec0768
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-03 20:26:38 +0700

    test CustomBottomBarWidget

--- lib/widgets/bottom_widget.dart
+++ lib/widgets/bottom_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/widgets/bottom_content_widget.dart';
 import 'package:flutter_project/widgets/code_content_widget.dart';
+import 'package:flutter_project/widgets/custom_bottom_bar_widget.dart';
 import 'package:flutter_project/widgets/form_field_content_widget.dart';
+import 'package:flutter_project/widgets/second_title_in_row.dart';
 
 class BottomWidget extends StatelessWidget {
   const BottomWidget({Key? key}) : super(key: key);
@@ -9,16 +10,26 @@ class BottomWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        Padding(
+      children: [
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 28),
           child: FormFieldContentWidget(),
         ),
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 28),
           child: CodeContentWidget(),
         ),
-        BottomContentWidget(),
+        // BottomContentWidget(),
+        CustomBottomBarWidget(
+          color: Theme.of(context).colorScheme.onPrimaryContainer,
+          padding: 28,
+          child: SecondTitleInRow(
+            firstTitle: "วันเสาร์",
+            firstPressed: () {},
+            secondTitle: "วันเสาร์",
+            secondPressed: () {},
+          ),
+        ),
       ],
     );
   }
