e2d2a461bc2f92fa7dc7bbc0e805cb8c614ba672
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-08 13:53:26 +0700

    basic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<int> getData() async {
+    await Future.delayed(const Duration(seconds: 4));
+    return 7;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Somethings',
-          style: Theme.of(context).textTheme.headline5,
+        child: FutureBuilder<int?>(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              int data = snapshot.data!;
+              return Text(
+                '⏳ $data',
+                style: Theme.of(context).textTheme.headline5,
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
         ),
       ),
     );
