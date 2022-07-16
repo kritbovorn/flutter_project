89d00ca64feb5a69ed99df6506038a7f859eddf1
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-16 12:09:50 +0700

    Copy from Narongphon

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/screens/home_screen.dart';
+import 'package:flutter_project/game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: HomeScreen(),
+      home: Game(),
     );
   }
 }
