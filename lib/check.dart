4fa12adb021213aea078f3bbdf168de89faaa521
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-18 21:55:56 +0700

    will show Dialog()

---
+++ lib/check.dart
@@ -0,0 +1,8 @@
+class Check {
+  int player;
+  List<int> matchs;
+  Check({
+    required this.player,
+    required this.matchs,
+  });
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_project/check.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -21,7 +22,8 @@ class _HomeScreenState extends State<HomeScreen> {
       isSwitchPlayer ? games[index] = "O" : games[index] = "X";
       isSwitchPlayer = !isSwitchPlayer;
 
-      _checkWinner();
+      matchIndexs = _checkWinner().matchs;
+      debugPrint(matchIndexs.toString());
     });
   }
 
@@ -34,39 +36,97 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void _checkWinner() {
+  Check _checkWinner() {
+    Check check = Check(player: 0, matchs: []);
     if (games[0] == games[1] && games[0] == games[2] && games[0] != "") {
-      matchIndexs = [0, 1, 2];
+      check.matchs = [0, 1, 2];
       _showWinnerDialog();
+      if (games[0] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[0] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[3] == games[4] && games[3] == games[5] && games[3] != "") {
-      matchIndexs = [3, 4, 5];
+      check.matchs = [3, 4, 5];
       _showWinnerDialog();
+      if (games[3] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[3] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[6] == games[7] && games[6] == games[8] && games[6] != "") {
-      matchIndexs = [6, 7, 8];
+      check.matchs = [6, 7, 8];
       _showWinnerDialog();
+      if (games[6] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[6] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[0] == games[3] && games[0] == games[6] && games[0] != "") {
-      matchIndexs = [0, 3, 6];
+      check.matchs = [0, 3, 6];
       _showWinnerDialog();
+      if (games[0] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[0] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[1] == games[4] && games[1] == games[7] && games[1] != "") {
-      matchIndexs = [1, 4, 7];
+      check.matchs = [1, 4, 7];
       _showWinnerDialog();
+      if (games[1] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[1] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[2] == games[5] && games[2] == games[8] && games[2] != "") {
-      matchIndexs = [2, 5, 8];
+      check.matchs = [2, 5, 8];
       _showWinnerDialog();
+      if (games[2] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[2] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[0] == games[4] && games[0] == games[8] && games[0] != "") {
-      matchIndexs = [0, 4, 8];
+      check.matchs = [0, 4, 8];
       _showWinnerDialog();
+      if (games[0] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[0] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
     if (games[2] == games[4] && games[2] == games[6] && games[2] != "") {
-      matchIndexs = [2, 4, 6];
+      check.matchs = [2, 4, 6];
       _showWinnerDialog();
+      if (games[2] == 'O') {
+        check.player = 1;
+        return check;
+      } else if (games[2] == 'X') {
+        check.player = 2;
+        return check;
+      }
     }
+    return check;
   }
 
   void _showWinnerDialog() {}
