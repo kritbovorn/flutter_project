33da9cd6784d12fd1e3781a7c50032392c068df1
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-20 01:01:15 +0700

    again

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,15 +9,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // List<Dar> cells = List.filled(10, DarkKeyboard);
+  List<int> numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+  ];
+  List<String> letters = [
+    "",
+    "ABC",
+    "DEF",
+    "GHI",
+    "JKL",
+    "MNO",
+    "PQRS",
+    "TUV",
+    "WXYZ"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Keyboard')),
-      body: Center(
-        child: DarkKeyboard(
-          number: 3,
-          letter: "ABC",
-          onPressed: () {},
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Expanded(
+              child: SizedBox.shrink(),
+            ),
+            Container(
+              height: 300,
+              color: const Color(0xFF323234),
+              child: Wrap(
+                children: List.generate(
+                    9,
+                    (index) => DarkKeyboard(
+                          letter: letters[index],
+                          number: numbers[index],
+                          onPressed: () {},
+                        )),
+              ),
+            )
+          ],
         ),
       ),
     );
