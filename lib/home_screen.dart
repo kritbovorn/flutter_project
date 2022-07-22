c2c8b19bba535a5c6a434f465d59ccd78be7b308
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-22 12:43:26 +0700

    separate code for Sex section

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,10 +2,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_project/alert_confirm_widget.dart';
-import 'package:flutter_project/button/button_widget.dart';
 import 'package:flutter_project/keyboard/keyboard_widget/dark_keyboard_widget.dart';
 import 'package:flutter_project/keyboard/keyboard_model/keyboard_model.dart';
-import 'package:flutter_project/text/text_widget.dart';
+import 'package:flutter_project/widgets/container_sex.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -88,59 +87,9 @@ class _HomeScreenState extends State<HomeScreen> {
               color: const Color.fromARGB(255, 108, 147, 44),
               width: MediaQuery.of(context).size.width,
               height: 200,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const TextWidget(title: 'With Icon'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const TextWidget(title: 'Sex:'),
-                      Stack(
-                        alignment: isSex
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(255, 226, 226, 226),
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            width: 260,
-                            height: 60,
-                          ),
-                          Row(
-                            children: [
-                              ButtonWidget(
-                                iconData: Icons.woman,
-                                title: 'Female',
-                                onPressed: () => setState(() => isSex = !isSex),
-                              ),
-                              ButtonWidget(
-                                iconData: Icons.man,
-                                title: 'Male',
-                                onPressed: () => setState(() => isSex = !isSex),
-                              ),
-                            ],
-                          ),
-                          ButtonWidget(
-                            primaryColor:
-                                const Color.fromARGB(255, 22, 84, 135),
-                            iconData: isSex ? Icons.man : Icons.woman,
-                            title: isSex ? 'Male' : 'FeMale',
-                            onPressed: () {},
-                            iconColor: Colors.white,
-                            textColor: Colors.white,
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  TextWidget(
-                      title: isSex
-                          ? 'Selected sex: Male'
-                          : 'Selected sex: Female'),
-                ],
+              child: ContainerSex(
+                isSex: isSex,
+                onPressed: () => setState(() => isSex = !isSex),
               ),
             ),
             Padding(
@@ -195,16 +144,14 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             if (isShowKeyboard)
               DarkKeyboardWidget(
-                  hasActive: hasActive,
-                  hasShowIcon: hasShowIcon,
-                  models: models,
-                  values: (value) {
-                    textEditingController.text = addResult(value);
-
-                    setState(() {
-                      changeState();
-                    });
-                  }),
+                hasActive: hasActive,
+                hasShowIcon: hasShowIcon,
+                models: models,
+                values: (value) {
+                  textEditingController.text = addResult(value);
+                  setState(() => changeState());
+                },
+              ),
           ],
         ),
       ),
---
+++ lib/widgets/container_sex.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:flutter_project/button/button_widget.dart';
+import 'package:flutter_project/text/text_widget.dart';
+
+class ContainerSex extends StatelessWidget {
+  final VoidCallback onPressed;
+  final bool isSex;
+  const ContainerSex({
+    Key? key,
+    required this.onPressed,
+    required this.isSex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const TextWidget(title: 'With Icon'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const TextWidget(title: 'Sex:'),
+            Stack(
+              alignment: isSex ? Alignment.centerRight : Alignment.centerLeft,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 226, 226, 226),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  width: 260,
+                  height: 60,
+                ),
+                Row(
+                  children: [
+                    ButtonWidget(
+                      iconData: Icons.woman,
+                      title: 'Female',
+                      onPressed: onPressed,
+                    ),
+                    ButtonWidget(
+                      iconData: Icons.man,
+                      title: 'Male',
+                      onPressed: onPressed,
+                    ),
+                  ],
+                ),
+                ButtonWidget(
+                  primaryColor: const Color.fromARGB(255, 22, 84, 135),
+                  iconData: isSex ? Icons.man : Icons.woman,
+                  title: isSex ? 'Male' : 'FeMale',
+                  onPressed: () {},
+                  iconColor: Colors.white,
+                  textColor: Colors.white,
+                ),
+              ],
+            )
+          ],
+        ),
+        TextWidget(
+            title: isSex ? 'Selected sex: Male' : 'Selected sex: Female'),
+      ],
+    );
+  }
+}
