3cb99f65405a75727df0939be75877a3b64c6302
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-23 11:04:21 +0700

    ### Perfect layout

--- lib/calendar/calendar_screen.dart
+++ lib/calendar/calendar_screen.dart
@@ -30,9 +30,75 @@ class _CalendarScreenState extends State<CalendarScreen> {
                 flex: 2,
                 child: Container(
                   color: Colors.red,
-                  child: ElevatedButton(
-                    onPressed: prin,
-                    child: const Text('print'),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 24,
+                        child: Container(
+                          color: Colors.indigo,
+                          child: LayoutBuilder(
+                            builder: (context, constraints) {
+                              double pw = constraints.maxWidth * 0.08;
+                              return Padding(
+                                padding: EdgeInsets.symmetric(horizontal: pw),
+                                child: Column(
+                                  // mainAxisAlignment: MainAxisAlignment.center,
+                                  // crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Expanded(
+                                      child: SizedBox.shrink(),
+                                    ),
+                                    Expanded(
+                                      flex: 3,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            '2565',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                              height: 0.5,
+                                            ),
+                                          ),
+                                          Row(
+                                            children: const [
+                                              Text(
+                                                'Mon,\t',
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 34,
+                                                  // height: 0,
+                                                ),
+                                              ),
+                                              Text(
+                                                'Mar\t',
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 34,
+                                                ),
+                                              ),
+                                              Text(
+                                                '16',
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 34,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
@@ -42,10 +108,56 @@ class _CalendarScreenState extends State<CalendarScreen> {
                   color: Colors.green,
                   child: Column(
                     children: [
+                      // *** March and Year
                       Expanded(
                         child: Container(
                           padding: EdgeInsets.zero,
                           color: Colors.blue,
+                          child: LayoutBuilder(
+                            builder: (context, constraints) {
+                              double pw = constraints.maxWidth * 0.04;
+                              return Padding(
+                                padding: EdgeInsets.symmetric(horizontal: pw),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.arrow_back_ios),
+                                      iconSize: 14,
+                                    ),
+                                    Row(
+                                      children: const [
+                                        Text(
+                                          'March',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 17,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 5,
+                                        ),
+                                        Text(
+                                          '2565',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 17,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      iconSize: 14,
+                                      icon: const Icon(Icons.arrow_forward_ios),
+                                    )
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
                         ),
                       ),
                       // *** Date Sun Sat Fri THU WED TUE MON
@@ -56,7 +168,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                             builder: (context, constraints) {
                               double ratio = (screenSize.width / 7) /
                                   (constraints.maxHeight);
-                              double wp = constraints.maxWidth * 0.03;
+                              double pw = constraints.maxWidth * 0.03;
                               // debugPrint(ratio.toString());
                               return Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
@@ -65,7 +177,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                                     physics:
                                         const NeverScrollableScrollPhysics(),
                                     padding:
-                                        EdgeInsets.symmetric(horizontal: wp),
+                                        EdgeInsets.symmetric(horizontal: pw),
                                     shrinkWrap: true,
                                     gridDelegate:
                                         SliverGridDelegateWithFixedCrossAxisCount(
@@ -98,14 +210,14 @@ class _CalendarScreenState extends State<CalendarScreen> {
                                     double ratio = (screenSize.width / 7) /
                                         (constraints.maxHeight / 5);
                                     double main = (constraints.maxHeight / 5);
-                                    double wp = (constraints.maxWidth * 0.03);
+                                    double pw = (constraints.maxWidth * 0.03);
 
                                     debugPrint(
                                         'Max height: ${constraints.maxHeight}');
                                     debugPrint(ratio.toString());
                                     return GridView.builder(
                                       padding:
-                                          EdgeInsets.symmetric(horizontal: wp),
+                                          EdgeInsets.symmetric(horizontal: pw),
                                       shrinkWrap: true,
                                       physics:
                                           const NeverScrollableScrollPhysics(),
