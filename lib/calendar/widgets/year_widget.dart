881a3034c2d1b981115ee69734bea87674ebe5d2
kritbovorn <ktaweeyossak@yahoo.com>
2022-07-28 14:42:09 +0700

    try layout year button

--- lib/calendar/widgets/year_widget.dart
+++ lib/calendar/widgets/year_widget.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class YearWidget extends StatelessWidget {
+class YearWidget extends StatefulWidget {
   const YearWidget({Key? key}) : super(key: key);
+
+  @override
+  State<YearWidget> createState() => _YearWidgetState();
+}
+
+class _YearWidgetState extends State<YearWidget> {
+  late DateTime yearChoice;
+  late List<int> yearLists;
+
+  @override
+  void initState() {
+    yearChoice = DateTime.now();
+    yearLists = List.generate(50, (index) => yearChoice.year + index);
+    debugPrint(yearLists.toString());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,10 +25,29 @@ class YearWidget extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 224, 221, 221),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               'Center',
               style: Theme.of(context).textTheme.headline3,
+            ),
+            Expanded(
+              child: Container(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 4,
+                    crossAxisSpacing: 4,
+                  ),
+                  itemCount: yearLists.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ElevatedButton(
+                      onPressed: () {},
+                      child: Text(yearLists[index].toString()),
+                    );
+                  },
+                ),
+              ),
             ),
           ],
         ),
